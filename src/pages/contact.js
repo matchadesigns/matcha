66038344877f3835f6ff2e389cc5e35a29b1d1c3
Joris <joris@saidani.org>
2020-05-06 17:52:21 +0200

    add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,38 @@
+/** @jsx jsx */
+import {graphql, useStaticQuery} from 'gatsby'
+import {Image, jsx} from 'theme-ui'
+import {BlockContent} from '../components/BlockContent'
+import {GraphQLErrorList} from '../components/GraphQLErrorList'
+import {Layout} from '../components/Layout'
+import {Main} from '../components/Layout/Main'
+import Seo from '../components/Seo'
+
+const ContactPage = () => {
+  const {
+    page: {title, _rawBody: body},
+    errors
+  } = useStaticQuery(graphql`
+    {
+      page: sanityPage(slug: {current: {eq: "contact"}}) {
+        title
+        _rawBody
+      }
+    }
+  `)
+  if (errors) {
+    return <GraphQLErrorList errors={errors} />
+  }
+
+  return (
+    <Layout>
+      <Seo title={title} />
+      <Main sx={{p: 4}}>
+        <h1 hidden>{title}</h1>
+        <div sx={{maxWidth: '600px', margin: 'auto'}}>
+          <BlockContent blocks={body} />
+        </div>
+      </Main>
+    </Layout>
+  )
+}
+export default ContactPage
