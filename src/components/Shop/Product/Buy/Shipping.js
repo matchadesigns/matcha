0a8e344094cbff5e5352b3c31352052e34b1040a
Joris <joris@saidani.org>
2020-05-26 00:28:12 +0200

    livraison

--- src/components/Shop/Product/Buy/Shipping.js
+++ src/components/Shop/Product/Buy/Shipping.js
@@ -3,6 +3,6 @@ import {jsx, Text} from 'theme-ui'
 
 export const Shipping = () => (
   <Text sx={{fontSize: 1, mt: 2}}>
-    <span sx={{color: 'primary'}}>Livraison offerte</span> à partir de 10€, en 3 jours chez vous
+    <span sx={{color: 'primary'}}>Livraison offerte</span> à partir de 100€, en 3 jours chez vous
   </Text>
 )
