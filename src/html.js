36d0b31b7d66ec8034688aa27ec9f92a4d35f98c
Joris <joris@saidani.org>
2020-06-08 19:37:12 +0200

    add mailchimp newsletter

--- src/html.js
+++ src/html.js
@@ -9,6 +9,7 @@ export default function HTML (props) {
         <meta httpEquiv='x-ua-compatible' content='ie=edge' />
         <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
         {props.headComponents}
+        <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/be2491f54097d34644dc66b3b/8e76913955ae9b08e5302a684.js");</script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
