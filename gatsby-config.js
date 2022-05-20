832c2035bf79e733fd53d184b49cf8574b5ec395
Siroj Inadias <joris@saidani.org>
2022-05-20 11:14:50 +0200

    remove preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 // Load variables from `.env` as soon as possible
 require('dotenv').config({
-  path: '.env'
+  path: '.env',
 })
 const path = require('path')
 const clientConfig = require('./client-config')
@@ -16,10 +16,9 @@ module.exports = {
     siteUrl: 'https://matchadesigns.com',
     description:
       "Mâtcha Designs est un duo nantais proposant des services en décoration d'intérieur et graphisme. Retrouvez également leurs objets déco design sur la boutique en ligne.",
-    author: '@matchadesigns'
+    author: '@matchadesigns',
   },
   plugins: [
-    'gatsby-plugin-preact',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
@@ -28,8 +27,8 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: path.join(__dirname, '/src/assets/images')
-      }
+        path: path.join(__dirname, '/src/assets/images'),
+      },
     },
     {
       resolve: 'gatsby-source-sanity',
@@ -37,25 +36,25 @@ module.exports = {
         ...clientConfig.sanity,
         token,
         watchMode: !isProd,
-        overlayDrafts: !isProd && token
-      }
+        overlayDrafts: !isProd && token,
+      },
     },
     'gatsby-plugin-theme-ui',
     {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: /assets/
-        }
-      }
+          include: /assets/,
+        },
+      },
     },
     {
       resolve: 'gatsby-plugin-snipcartv3',
       options: {
         apiKey: process.env.SNIPCART_APIKEY,
         js: '/snipcart.3.2.0.js',
-        styles: '/snipcart.3.2.0.css'
-      }
+        styles: '/snipcart.3.2.0.css',
+      },
     },
     {
       resolve: 'gatsby-plugin-manifest',
@@ -66,8 +65,8 @@ module.exports = {
         background_color: '#B67D20',
         theme_color: '#B67D20',
         display: 'minimal-ui',
-        icon: 'src/assets/images/icon.png' // This path is relative to the root of the site.
-      }
+        icon: 'src/assets/images/icon.png', // This path is relative to the root of the site.
+      },
     },
     {
       resolve: 'gatsby-plugin-nprogress',
@@ -75,15 +74,15 @@ module.exports = {
         // Setting a color is optional.
         color: '#B67D20',
         // Disable the loading spinner.
-        showSpinner: false
-      }
+        showSpinner: false,
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: path.join(__dirname, 'src', 'assets', 'images')
-      }
+        path: path.join(__dirname, 'src', 'assets', 'images'),
+      },
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
@@ -93,17 +92,17 @@ module.exports = {
       options: {
         appId: process.env.GATSBY_ALGOLIA_APP_ID,
         apiKey: process.env.ALGOLIA_API_KEY,
-        queries
+        queries,
         // chunkSize: 1000, // default: 1000
-      }
+      },
     },
     'gatsby-plugin-zeit-now',
     'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-google-gtag',
       options: {
-        trackingIds: [process.env.GOOGLE_ANALYTICS]
-      }
-    }
-  ]
+        trackingIds: [process.env.GOOGLE_ANALYTICS],
+      },
+    },
+  ],
 }
