159046e961805e78cb1375fcfef7e48228935025
Siroj Inadias <joris@saidani.org>
2025-03-29 12:55:03 +0100

    Update Title.js

--- src/components/Homepage/Title.js
+++ src/components/Homepage/Title.js
@@ -1,13 +1,13 @@
 /** @jsx jsx */
 
-import {jsx} from 'theme-ui'
-import {Fragment} from 'react'
-import Logo from '../../assets/svg/logo.svg'
+import { jsx } from "theme-ui";
+import { Fragment } from "react";
+import Logo from "../../assets/svg/logo2025.svg";
 // import MatchaCurved from '../../assets/svg/matcha_curved.svg'
 
 export const Title = () => (
   <Fragment>
-    <Logo sx={{width: '35vmin', mt: [4, 6], mb: 3, zIndex: 1}} />
+    <Logo sx={{ width: "35vmin", mt: [4, 6], mb: 3, zIndex: 1 }} />
     <h1 hidden>Mâtcha Designs</h1>
   </Fragment>
-)
+);
