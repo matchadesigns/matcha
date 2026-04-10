58888e2ae77a80a64110207d63a6b296b9df5473
Siroj Inadias <contact.studiomatcha@gmail.com>
2026-04-10 10:09:06 +0200

    Update shippingCosts.ts

--- api/shippingCosts.ts
+++ api/shippingCosts.ts
@@ -1,68 +1,61 @@
-import {createClient} from '@sanity/client'
-import { VercelRequest, VercelResponse } from '@vercel/node'
-import { sanity } from '../client-config'
+import { createClient } from "@sanity/client";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { sanity } from "../client-config";
 
-export default function handler(
-  req: VercelRequest,
-  res: VercelResponse,
-) {
-
+export default function handler(req: VercelRequest, res: VercelResponse) {
   if (!(req && req.body && req.body.content && req.body.content.items)) {
     res.status(200).json({
       errors: [
         {
-          key: 'invalid_content',
-          message: 'The cart is invalid'
-        }
-      ]
-    })
-    return false
+          key: "invalid_content",
+          message: "The cart is invalid",
+        },
+      ],
+    });
+    return false;
   }
 
   const totalPrice =
     req.body.content.items.length > 0
       ? req.body.content.items
-          .map(({totalPrice}) => totalPrice)
+          .map(({ totalPrice }) => totalPrice)
           .reduce((pv, cv) => pv + cv, 0)
-      : 0
-  let shippingCosts = 0
+      : 0;
+  let shippingCosts = 0;
   if (totalPrice > 0 && totalPrice <= 10) {
-    shippingCosts = 1.5
+    shippingCosts = 6.95;
   } else if (totalPrice > 10 && totalPrice < 100) {
-    shippingCosts = 6.95
+    shippingCosts = 6.95;
   } else if (totalPrice >= 100) {
-    shippingCosts = 0
+    shippingCosts = 0;
   }
 
   const client = createClient({
     projectId: sanity.projectId,
     dataset: sanity.dataset,
-    apiVersion: '2023-05-03', // 2021-03-25
-  })
+    apiVersion: "2023-05-03", // 2021-03-25
+  });
 
-client.getDocument('siteSettings').then((settings) => {
-  if(settings?.isFreeShipping === true) {
-    res.status(200).json({
-      rates: [
-        {
-          cost: 0,
-          description:
-            'Frais de ports offerts !'
-        }
-      ]
-    })
-  }
-  else {
-    res.status(200).json({
-      rates: [
-        {
-          cost: shippingCosts,
-          description:
-            'Frais de ports offerts pour les commandes de plus de 100€.'
-        }
-      ]
-    })
-  }
-})
-  
+  client.getDocument("siteSettings").then((settings) => {
+    if (settings?.isFreeShipping === true) {
+      res.status(200).json({
+        rates: [
+          {
+            cost: 0,
+            description: "Frais de ports offerts !",
+          },
+        ],
+      });
+    } else {
+      res.status(200).json({
+        rates: [
+          {
+            cost: shippingCosts,
+            description:
+              "Frais de ports offerts pour les commandes de plus de 100€.",
+          },
+        ],
+      });
+    }
+  });
 }
