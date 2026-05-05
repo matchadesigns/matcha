f0019f8e46a67edabd4f9d5f3cb0a56e1a1383bf
Siroj Inadias <contact.studiomatcha@gmail.com>
2026-05-05 17:41:52 +0200

    fix: image flash

--- src/components/Shop/ProductCard/Image.js
+++ src/components/Shop/ProductCard/Image.js
@@ -1,6 +1,7 @@
 /* eslint-disable multiline-ternary */
 /** @jsx jsx */
 import {jsx} from 'theme-ui'
+import {useMemo} from 'react'
 import {motion} from 'framer-motion'
 import {sanityConfig} from '../../../../sanity-config'
 import {GatsbyImage} from 'gatsby-plugin-image'
@@ -8,28 +9,14 @@ import {getGatsbyImageData} from 'gatsby-source-sanity'
 import {Link} from 'gatsby'
 
 export const Image = ({image, link}) => {
-  const Image = () => (
-    <GatsbyImage
-      image={getGatsbyImageData(
-        image,
-        {
-          maxWidth: 300
-        },
-        sanityConfig
-      )}
-      alt={'Image'}
-      sx={{variant: 'images.card'}}
-    />
+  const imageData = useMemo(
+    () => getGatsbyImageData(image, {maxWidth: 300, placeholder: 'blurred'}, sanityConfig),
+    [image]
   )
+  const inner = <GatsbyImage image={imageData} alt='Image' sx={{variant: 'images.card'}} />
   return (
     <motion.div whileHover={{scale: 1.02}} whileTap={{scale: 0.9}}>
-      {link ? (
-        <Link to={link}>
-          <Image />
-        </Link>
-      ) : (
-        <Image />
-      )}
+      {link ? <Link to={link}>{inner}</Link> : inner}
     </motion.div>
   )
 }
